09f7e0b59826bdb49b5f9fab8aead051ccb811b3
Adam Wujek <adam.wujek@cern.ch>
2018-11-20 17:00:44 +0100

    userspace/libwr: add verbosity when double lock/unlock of shmem
    Also increase all *_SHMEM_VERSION
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/libwr/include/libwr/rtu_shmem.h
+++ userspace/libwr/include/libwr/rtu_shmem.h
@@ -125,8 +125,7 @@ struct rtu_vlan_table_entry {
 };
 
 /* This is the overall structure stored in shared memory */
-#define RTU_SHMEM_VERSION 2 /* Version 2, added filters_offset and
-			     * vlans_offset */
+#define RTU_SHMEM_VERSION 3 /* Version 3, changed wrs_shm_head */
 struct rtu_shmem_header {
 	struct rtu_filtering_entry *filters;
 	struct rtu_vlan_table_entry *vlans;

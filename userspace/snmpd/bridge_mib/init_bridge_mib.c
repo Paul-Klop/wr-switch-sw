241ce107f25fde30ef125d0e644927508cefff56
Michal Wasiak <michal.wasiak@gmail.com>
2020-10-07 16:53:07 +0200

    snmp/bridge_mib: add Makefiles
    Signed-off-by: Michal Wasiak <michal.wasiak@gmail.com>

---
+++ userspace/snmpd/bridge_mib/init_bridge_mib.c
@@ -0,0 +1,15 @@
+/*
+ * A global (library-wide) init function to register several things
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+
+/* The sub-init functions */
+#include "wrsSnmp.h"
+#include "snmp_shmem.h"
+
+void init_bridge_mib(void)
+{
+
+}

8886548b7fdc5b4e5abb6887f4f6decb8d3dd933
Michal Wasiak <michal.wasiak@gmail.com>
2020-10-14 02:15:07 +0200

    userspace/libwr: add function to change order o bits in a portmask
    Used by SNMP
    
    Signed-off-by: Michal Wasiak <michal.wasiak@gmail.com>

--- userspace/libwr/util.c
+++ userspace/libwr/util.c
@@ -144,3 +144,24 @@ unsigned int bitCount (unsigned int value) {
 	return count;
 }
 
+/* Convert bitmask of ports from notation used by RTU (lowest port on lowest bit)
+ * to notation used by SNMP (lowrst port, most significant bit) */
+void convert_portmask_to_snmp_bitmask(int nports, uint32_t port_mask, char *bitmask, size_t *snmp_bitmask_len)
+{
+    int i;
+
+    /* round up number of ports to a number dividable by 8 */
+    nports = 8 * ((nports + 7) / 8);
+    for (i = 0; i < nports;i++){
+	*bitmask |= port_mask & 1;
+	port_mask >>= 1;
+	/* jump to next char */
+	if (i % 8 == 7) {
+	    bitmask++;
+	    *bitmask = 0;
+	}
+	*bitmask <<= 1;
+    }
+    /* count the number of bytes used */
+    *snmp_bitmask_len = nports / 8;
+}

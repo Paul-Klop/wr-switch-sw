fc05e2250cf41cea1da373ff7986ffe42e57bf3c
Adam Wujek <dev_public@wujek.eu>
2020-12-19 23:44:14 +0100

    userspace/snmpd: changes due to the uplift of snmpd to 5.9
    Signed-off-by: Adam Wujek <dev_public@wujek.eu>

--- userspace/snmpd/bridge_mib/dot1qVlanStaticTable/dot1qVlanStaticTable_interface.c
+++ userspace/snmpd/bridge_mib/dot1qVlanStaticTable/dot1qVlanStaticTable_interface.c
@@ -41,9 +41,9 @@
 
 #include "dot1qVlanStaticTable_interface.h"
 
-netsnmp_feature_require(baby_steps)
-netsnmp_feature_require(row_merge)
-netsnmp_feature_require(check_all_requests_error)
+netsnmp_feature_require(baby_steps);
+netsnmp_feature_require(row_merge);
+netsnmp_feature_require(check_all_requests_error);
 
 #include <ctype.h>
 
